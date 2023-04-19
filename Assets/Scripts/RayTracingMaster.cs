4d916f44a43da2ad4f1bc4dffa48e3eee3ed2e82
김건우 <vsado531@gmail.com>
2023-04-19 15:57:34 +0900

    Scene and materials

--- Assets/Scripts/RayTracingMaster.cs
+++ Assets/Scripts/RayTracingMaster.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class RayTracingMaster : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -11,14 +12,22 @@ public class RayTracingMaster : MonoBehaviour
     private Texture SkyboxTexture;
     [SerializeField]
     private Light DirectionalLight;
-    
+
+    [SerializeField, Range(0.0f, 1.9f)] private float _globalIllumination = 1.0f;
     [SerializeField, Range(1, 9)] private int _reflections;
-    
+
+    [Header("Spheres")]
+    [SerializeField] private int _sphereSeed = 111;
+    [SerializeField] private Vector2 _sphereRadius = new Vector2(3.0f, 8.0f);
+    [SerializeField, Range(0, 200)] private int _spheresMax = 146;
+    [SerializeField] private float _spherePlacementRadius = 100.0f;
 
     private RenderTexture _target;
+    private RenderTexture _converged;
     private Camera _camera;
     private uint _currentSample = 0;
     private Material _addMaterial;
+    private ComputeBuffer _sphereBuffer;
 
 
     //자동 콜 : 카메라 렌더링이 끝날때 마다.
@@ -29,6 +38,19 @@ public class RayTracingMaster : MonoBehaviour
         Render(destination);
     }
 
+    private void OnEnable()
+    {
+        _currentSample = 0;
+        SetupScene();
+    }
+
+    private void OnDisable()
+    {
+        if (_sphereBuffer != null)
+        {
+            _sphereBuffer.Release();
+        }
+    }
 
     private void Render(RenderTexture destination)
     {
@@ -37,24 +59,23 @@ public class RayTracingMaster : MonoBehaviour
         //결과 텍스쳐
         RayTracingShader.SetTexture(0, "Result", _target);
 
-
-        int threadGroupsX = Mathf.CeilToInt(Screen.width / 8.0f);
-        int threadGroupsY = Mathf.CeilToInt(Screen.height / 8.0f);
+        float threadGroupSizeX = 8.0f;
+        float threadGroupSizeY = 8.0f;
+        int threadGroupsX = Mathf.CeilToInt(Screen.width / threadGroupSizeX);
+        int threadGroupsY = Mathf.CeilToInt(Screen.height / threadGroupSizeY);
 
         //처리
         RayTracingShader.Dispatch(0, threadGroupsX, threadGroupsY, 1);
 
         //progressive smapling by addShader
-        if(_addMaterial == null)
+        if (_addMaterial == null)
         {
             _addMaterial = new Material(Shader.Find("Hidden/AddShader"));
         }
 
         _addMaterial.SetFloat("_Sample", _currentSample);
-
-         
-        //결과 텍스쳐를 스크린에 그리기
-        Graphics.Blit(_target, destination, _addMaterial);
+        Graphics.Blit(_target, _converged, _addMaterial);
+        Graphics.Blit(_converged, destination);
 
         _currentSample++;
     }
@@ -62,8 +83,24 @@ public class RayTracingMaster : MonoBehaviour
 
     private void InitRenderTexture()
     {
+        if (_converged == null || _converged.height != Screen.height || _converged.width != Screen.width)
+        {
+            _currentSample = 0;
+
+            if (_converged != null)
+            {
+                _converged.Release();
+            }
+
+            _converged = new RenderTexture(Screen.width, Screen.height, 0,
+                RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+            _converged.enableRandomWrite = true;
+            _converged.Create();
+        }
+
         if (_target == null || _target.width != Screen.width || _target.height != Screen.height)
         {
+            _currentSample = 0;
             //이미 할당된 텍스처가 있으면 비우기
             if (_target != null)
                 _target.Release();
@@ -95,6 +132,15 @@ public class RayTracingMaster : MonoBehaviour
         //Directional  Light
         Vector3 l = DirectionalLight.transform.forward;
         RayTracingShader.SetVector("_DirectionalLight", new Vector4(l.x, l.y, l.z, DirectionalLight.intensity));
+
+        //Spheres
+        RayTracingShader.SetBuffer(0, "_Spheres", _sphereBuffer);
+
+        //Randomness seed
+        RayTracingShader.SetFloat("_Seed", Random.value);
+
+        //GlobalIllumination
+        RayTracingShader.SetFloat("_GlobalIllumination", _globalIllumination);
     }
 
     private void Awake()
@@ -106,18 +152,19 @@ public class RayTracingMaster : MonoBehaviour
     private void OnValidate()
     {
         _currentSample = 0;
+        SetupScene();
     }
 
     private void Update()
     {
         //Reset _currentSample count if camera has moved
-        if(transform.hasChanged)
+        if (transform.hasChanged)
         {
             _currentSample = 0;
             transform.hasChanged = false;
         }
 
-        if(DirectionalLight.transform.hasChanged)
+        if (DirectionalLight.transform.hasChanged)
         {
             _currentSample = 0;
             DirectionalLight.transform.hasChanged = false;
@@ -129,5 +176,61 @@ public class RayTracingMaster : MonoBehaviour
 
     }
 
-}
 
+    private void SetupScene()
+    {
+        Random.InitState(_sphereSeed);
+        List<Sphere> spheres = new List<Sphere>();
+        // Add a number of random spheres
+        for (int i = 0; i < _spheresMax; i++)
+        {
+            Sphere sphere = new Sphere();
+            // Create random position and radius
+            sphere.radius = _sphereRadius.x + Random.value * (_sphereRadius.y - _sphereRadius.x);
+            Vector2 randomPos = Random.insideUnitCircle * _spherePlacementRadius;
+            sphere.position = new Vector3(randomPos.x, sphere.radius, randomPos.y);
+            // Reject spheres that are intersecting others
+            foreach (Sphere other in spheres)
+            {
+                float minDist = sphere.radius + other.radius;
+                if (Vector3.SqrMagnitude(sphere.position - other.position) < minDist * minDist)
+                {
+                    goto SkipSphere;
+                }
+
+            }
+
+            // Albedo, specular color and smoothness and emission
+            Color color = Random.ColorHSV();
+            float chance = Random.value;
+            if (chance < 0.85f)
+            {
+                bool metal = chance < 0.7f;
+                sphere.albedo = metal ? Vector4.zero : new Vector4(color.r, color.g, color.b);
+                sphere.specular = metal ? new Vector4(color.r, color.g, color.b) : new Vector4(0.04f, 0.04f, 0.04f);
+                sphere.smoothness = Random.value;
+            }
+            else
+            {
+                Color emission = Random.ColorHSV(0, 1, 0, 1, 1.0f, 3.0f);
+                sphere.emission = new Vector3(emission.r, emission.g, emission.b);
+            }
+
+            // Add the sphere to the list
+            spheres.Add(sphere);
+        SkipSphere:
+            continue;
+        }
+
+
+        // Compute size of Sphere struct in bytes
+        int strideSize = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Sphere));
+        // Create and ssign to compute buffer
+        if (_sphereBuffer != null)
+        {
+            _sphereBuffer.Release();
+        }
+        _sphereBuffer = new ComputeBuffer(spheres.Count, strideSize);
+        _sphereBuffer.SetData(spheres);
+    }
+}
